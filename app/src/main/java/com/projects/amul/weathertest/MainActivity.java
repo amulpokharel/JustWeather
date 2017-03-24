b16a69e90cb250210b788309222570360a52819c
Amul Pokharel <amulpok@Ifrit.local>
2017-03-24 15:46:19 -0400

    gets coarse location, although casts into int.  might need to check double?

--- app/src/main/java/com/projects/amul/weathertest/MainActivity.java
+++ app/src/main/java/com/projects/amul/weathertest/MainActivity.java
@@ -1,10 +1,15 @@
 package com.projects.amul.weathertest;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,11 +28,17 @@ import java.net.URL;
 
 import butterknife.BindView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    @BindView(R.id.weatherText) TextView weather;
+    @BindView(R.id.weatherText)
+    TextView weather;
 
-    @BindView(R.id.weatherIcon) ImageView weatherIcon;
+    @BindView(R.id.weatherIcon)
+    ImageView weatherIcon;
+
+    private int lng = 0;
+    private int lat = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +47,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //set up location manager + check if enabled
-        LocationManager location = (LocationManager) getSystemService(LOCATION_SERVICE);
-        boolean enabled = location.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        int lng = 0, lat = 0;
-
-        //prompt user to enable
-        if(!enabled){
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
+        LocationManager location = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        location.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
         try {
             Location loc = location.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             lng = (int)loc.getLongitude();
@@ -77,6 +91,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lng = (int)location.getLongitude();
+        lat = (int)location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //prompt user to enable GPS
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
