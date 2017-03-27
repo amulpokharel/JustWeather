8abc18a5a1bd44985fe9a3c647670394696511ee
Amul Pokharel <amulpokharel@users.noreply.github.com>
2017-03-27 19:47:31 -0400

    split up download task and location provider, added classes to handle json (generated from jsonschema2pojo).  used gson to parse the result from http req.

---
+++ app/src/main/java/com/projects/amul/weathertest/modules/LocationProvider.java
@@ -0,0 +1,94 @@
+package com.projects.amul.weathertest.modules;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by amulpok on 3/27/17.
+ */
+
+public class LocationProvider implements LocationListener {
+
+    private static Location loc;
+    private double lng = 0, lat = 0;
+    private LocationManager locationManager;
+    private Context context;
+
+    public LocationProvider(){
+
+    }
+
+
+    public LocationProvider(Context context) {
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        this.context = context;
+
+
+        //check permissions to sate locationmanager
+
+
+        //set location source, and get initial long/lat
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        try {
+            loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            lng = loc.getLongitude();
+            lat = loc.getLatitude();
+        }
+        catch(SecurityException e)
+        {
+
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    public double getLongitude(){
+        return lng;
+    }
+
+    public double getLatitude(){
+        return lat;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lng = location.getLongitude();
+        lat = location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //prompt user to enable GPS
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        context.startActivity(intent);
+    }
+}
