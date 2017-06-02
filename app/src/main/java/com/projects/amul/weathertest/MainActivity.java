eb7505daeef2f53309e64b825d58ef1a52593d5d
Amul Pokharel <amulpokharel@users.noreply.github.com>
2017-06-02 16:00:12 -0400

    hid the nav bar + notification bar

--- app/src/main/java/com/projects/amul/weathertest/MainActivity.java
+++ app/src/main/java/com/projects/amul/weathertest/MainActivity.java
@@ -8,6 +8,7 @@ package com.projects.amul.weathertest;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fm.beginTransaction().replace(R.id.mainFrame, new WeatherFragment()).commit();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
 }
