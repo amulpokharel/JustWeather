61154e2525ad8375c9cc0404c1f22e792cc68215
Amul Pokharel <amulpokharel@users.noreply.github.com>
2017-06-05 09:37:41 -0400

    Retains the fragment instead of blowing it up each time on config change

--- app/src/main/java/com/projects/amul/weathertest/MainActivity.java
+++ app/src/main/java/com/projects/amul/weathertest/MainActivity.java
@@ -5,7 +5,6 @@
 package com.projects.amul.weathertest;
 
 
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -13,20 +12,24 @@ import android.view.View;
 
 
 public class MainActivity extends FragmentActivity {
-    FragmentManager fm = getSupportFragmentManager();
-    WeatherFragment weather_frag;
+
+    private static final String SAVED_WEATHER = "WeatherFragment";
+    private WeatherFragment weather_frag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(savedInstanceState == null){
+
+        FragmentManager fm = getSupportFragmentManager();
+        weather_frag = (WeatherFragment) fm.findFragmentByTag(SAVED_WEATHER);
+
+        if(weather_frag == null){
             weather_frag = new WeatherFragment();
+            fm.beginTransaction().replace(R.id.mainFrame, weather_frag, SAVED_WEATHER).commit();
         }
-        else{
-            weather_frag = (WeatherFragment)fm.getFragment(savedInstanceState, "weather fragment");
-        }
-        fm.beginTransaction().replace(R.id.mainFrame, weather_frag).commit();
+
+
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -37,7 +40,6 @@ public class MainActivity extends FragmentActivity {
     }
 
     public void onSaveInstanceState(Bundle outState){
-        fm.putFragment(outState,"weather fragment",weather_frag);
         super.onSaveInstanceState(outState);
     }
 
